3d66f4e7ded9ea8aa9d6c383a0403987f187a8cc
Arunkumar2708 <arunkumarbsc105@gmail.com>
2023-08-03 15:08:51 +0530

    flie

---
+++ Categories/Category-side/Add-Category/Add-Category.js
@@ -0,0 +1,24 @@
+
+let chosenimage = document.getElementById('chosen-image')
+let uploadButton = document.getElementById('uploadButton')
+let fileName = document.getElementById('file-name')
+
+uploadButton.onchange = () => {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadButton.files[0]);
+    console.log(uploadButton.files[0]);
+    reader.onload = () => {
+        chosenimage.setAttribute("src", reader.result);
+
+    }
+}
+
+
+
+    let CategoryMove = document.querySelector('#Category-move');       
+    CategoryMove.addEventListener('click', function (e) {
+         e.preventDefault();
+         let firstInput=document.getElementById('first-input').value;
+             console.log(firstInput);        
+             location.href = "../Category.html";
+    })
---
+++ Categories/sub-category/Sub-category.js
@@ -0,0 +1,17 @@
+let AddProducts = document.querySelector('#Add-products');
+AddProducts.addEventListener('click', function (e) {
+    e.preventDefault();
+    location.href = "./Add-View-category/add-Sub-Category.html";
+
+})
+
+let ViewDetails = document.querySelector('#view-details');
+ViewDetails.addEventListener('click', function (e) {
+    e.preventDefault();
+    location.href = "./Add-View-category/View-Sub-Category-details.html";
+
+})
+
+
+
+ 
